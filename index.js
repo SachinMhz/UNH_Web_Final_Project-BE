1db0ae15db79573f5f863a98b0400d7d785114f5
SachinMhz <sachin.mhz.sm@gmail.com>
2024-04-23 21:55:14 -0400

    Refactored

--- index.js
+++ index.js
@@ -1,85 +1,18 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
-http
-  .createServer((req, res) => {
-    console.log(req.url);
-    const {url} = req;
-    if (req.url == "/") {
-      console.log("in the root");
-      fs.readFile(
-        path.join(__dirname, "public", "index.html"),
-        (err, content) => {
-          if (err) throw err;
 
-          res.writeHead(200, { "Content-Type": "text/html" });
-
-          res.end(content);
-        }
-      );
-    } else if (req.url == "/style.css") {
-      console.log("in the css");
-      fs.readFile(
-        path.join(__dirname, "public", "style.css"),
-        (err, content) => {
-          if (err) throw err;
-
-          res.writeHead(200, { "Content-Type": "text/css" });
-
-          res.end(content);
-        }
-      );
-    } else if (req.url == "/about") {
-      fs.readFile(
-        path.join(__dirname, "public", "about.html"),
-        (err, content) => {
-          if (err) throw err;
-
-          res.writeHead(200, { "Content-Type": "text/html" });
-
-          res.end(content);
-        }
-      );
-    } else if (url.includes("/images/")) {
-      console.log("in the images");
-      const fileName = url.substring(8);
-      console.log(fileName);
-      serveStaticFile(res, fileName);
-    } else if (req.url == "/api") {
-      fs.readFile(path.join(__dirname, "public", "db.json"), (err, content) => {
-        if (err) throw err;
-
-        res.writeHead(200, { "Content-Type": "application/json" });
-
-        res.end(content);
-      });
-    } else {
-      res.write("Nothing changed");
-    }
-  })
-  .listen(5959, () => console.log("server running"));
-
-function serveStaticFile(res, fileName) {
-  const filePath = path.join(__dirname, "public", "images", fileName);
-  fs.readFile(filePath, (err, data) => {
-    if (err) {
-      res.writeHead(404);
-      res.end("Not Found");
-    } else {
-      const contentType = getContentType(filePath);
-      res.writeHead(200, { "Content-Type": contentType });
-      res.end(data);
-    }
-  });
-}
+const PORT = 8523;
 
 function getContentType(filePath) {
   const extname = path.extname(filePath);
   switch (extname) {
-    case ".js":
-      return "text/javascript";
+    case ".html":
+      return "text/html";
     case ".css":
       return "text/css";
+    case ".js":
+      return "text/javascript";
     case ".jpg":
     case ".jpeg":
       return "image/jpeg";
@@ -91,3 +24,41 @@ function getContentType(filePath) {
       return "application/octet-stream";
   }
 }
+
+function readFile(res, filePath) {
+  const contentType = getContentType(filePath);
+  fs.readFile(filePath, (err, content) => {
+    if (err) throw err;
+
+    res.writeHead(200, { "Content-Type": contentType });
+    res.end(content);
+  });
+}
+
+http
+  .createServer((req, res) => {
+    const { url } = req;
+
+    if (url == "/") {
+      const filePath = path.join(__dirname, "public", "index.html");
+      readFile(res, filePath);
+    } else if (url == "/style.css") {
+      const filePath = path.join(__dirname, "public", "style.css");
+      readFile(res, filePath);
+    } else if (url.includes("/images/")) {
+      const fileName = url.substring(8);
+      const filePath = path.join(__dirname, "public", "images", fileName);
+      readFile(res, filePath);
+    } else if (url == "/api") {
+      fs.readFile(path.join(__dirname, "public", "db.json"), (err, content) => {
+        if (err) throw err;
+
+        res.writeHead(200, { "Content-Type": "application/json" });
+        res.end(content);
+      });
+    } else {
+      // File not found error.
+      res.write("Nothing changed");
+    }
+  })
+  .listen(PORT, () => console.log(`server running at port: ${PORT}`));
