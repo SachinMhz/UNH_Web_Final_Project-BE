e4309e0e8f2ad22d2c5b089427c7aa71a1ba2dfd
SachinMhz <sachin.mhz.sm@gmail.com>
2024-04-23 21:29:32 -0400

    node-js

--- index.js
+++ index.js
@@ -1,71 +1,93 @@
-import { readFile } from "fs";
-import { join } from "path";
-import { createServer } from "http";
-import { MongoClient, ServerApiVersion } from "mongodb";
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+http
+  .createServer((req, res) => {
+    console.log(req.url);
+    const {url} = req;
+    if (req.url == "/") {
+      console.log("in the root");
+      fs.readFile(
+        path.join(__dirname, "public", "index.html"),
+        (err, content) => {
+          if (err) throw err;
 
-const uri =
-  "mongodb+srv://maharjan:maharjan123@cluster0.kmul092.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+          res.writeHead(200, { "Content-Type": "text/html" });
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  },
-});
+          res.end(content);
+        }
+      );
+    } else if (req.url == "/style.css") {
+      console.log("in the css");
+      fs.readFile(
+        path.join(__dirname, "public", "style.css"),
+        (err, content) => {
+          if (err) throw err;
 
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+          res.writeHead(200, { "Content-Type": "text/css" });
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+          res.end(content);
+        }
+      );
+    } else if (req.url == "/about") {
+      fs.readFile(
+        path.join(__dirname, "public", "about.html"),
+        (err, content) => {
+          if (err) throw err;
 
-    let movies = await findOneListingByName("movie");
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
-    console.log(movies);
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
+          res.writeHead(200, { "Content-Type": "text/html" });
 
-async function findOneListingByName( nameOfListing) {
-  const result = await client
-    .db("sample_mflix")
-    .collection("movies")
-    .findOne({ type: nameOfListing });
+          res.end(content);
+        }
+      );
+    } else if (url.includes("/images/")) {
+      console.log("in the images");
+      const fileName = url.substring(8);
+      console.log(fileName);
+      serveStaticFile(res, fileName);
+    } else if (req.url == "/api") {
+      fs.readFile(path.join(__dirname, "public", "db.json"), (err, content) => {
+        if (err) throw err;
 
-  if (result) {
-    console.log(
-      `Found a listing in the collection with the name '${nameOfListing}':`
-    );
-    console.log(result);
-  } else {
-    console.log(`No listings found with the name '${nameOfListing}'`);
-  }
-}
+        res.writeHead(200, { "Content-Type": "application/json" });
 
-run().catch(console.dir);
-
-
-
-
-createServer((req, res) => {
-    if(req.url == "/"){
-        readFile(join(__dirname,'/public','index.html'), 'utf-8', (err, data)=>{
-            if (err) throw err;
-
-            res.writeHead(200, {"Content-Type":"text/HTML"});
-            res.end(data);
-        })
-    }else{
-        res.write("not found");
-        res.end();
+        res.end(content);
+      });
+    } else {
+      res.write("Nothing changed");
     }
   })
   .listen(5959, () => console.log("server running"));
+
+function serveStaticFile(res, fileName) {
+  const filePath = path.join(__dirname, "public", "images", fileName);
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end("Not Found");
+    } else {
+      const contentType = getContentType(filePath);
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(data);
+    }
+  });
+}
+
+function getContentType(filePath) {
+  const extname = path.extname(filePath);
+  switch (extname) {
+    case ".js":
+      return "text/javascript";
+    case ".css":
+      return "text/css";
+    case ".jpg":
+    case ".jpeg":
+      return "image/jpeg";
+    case ".png":
+      return "image/png";
+    case ".gif":
+      return "image/gif";
+    default:
+      return "application/octet-stream";
+  }
+}
