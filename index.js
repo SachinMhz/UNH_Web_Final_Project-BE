b71bdb8243cc5bb8d5c1d42fb4db091103529173
SachinMhz <sachin.mhz.sm@gmail.com>
2024-04-23 22:22:40 -0400

    Add Mongo DB

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const PORT = 8523;
+const URI =
+  "mongodb+srv://maharjan:maharjan123@cluster0.kmul092.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 function getContentType(filePath) {
   const extname = path.extname(filePath);
@@ -35,30 +47,49 @@ function readFile(res, filePath) {
   });
 }
 
-http
-  .createServer((req, res) => {
-    const { url } = req;
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Successfully connected to MongoDB!");
 
-    if (url == "/") {
-      const filePath = path.join(__dirname, "public", "index.html");
-      readFile(res, filePath);
-    } else if (url == "/style.css") {
-      const filePath = path.join(__dirname, "public", "style.css");
-      readFile(res, filePath);
-    } else if (url.includes("/images/")) {
-      const fileName = url.substring(8);
-      const filePath = path.join(__dirname, "public", "images", fileName);
-      readFile(res, filePath);
-    } else if (url == "/api") {
-      fs.readFile(path.join(__dirname, "public", "db.json"), (err, content) => {
-        if (err) throw err;
+    http
+      .createServer(async (req, res) => {
+        const { url } = req;
 
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(content);
-      });
-    } else {
-      // File not found error.
-      res.write("Nothing changed");
-    }
-  })
-  .listen(PORT, () => console.log(`server running at port: ${PORT}`));
+        if (url == "/") {
+          const filePath = path.join(__dirname, "public", "index.html");
+          readFile(res, filePath);
+        } else if (url == "/style.css") {
+          const filePath = path.join(__dirname, "public", "style.css");
+          readFile(res, filePath);
+        } else if (url.includes("/images/")) {
+          const fileName = url.substring(8);
+          const filePath = path.join(__dirname, "public", "images", fileName);
+          readFile(res, filePath);
+        } else if (url == "/api/products") {
+          const cursor = await client
+            .db("bookdb")
+            .collection("bookcollection")
+            .find({});
+          const result = await cursor.toArray();
+          const response = { data: result, status: 200, message: "success" };
+
+          res.writeHead(200, { "Content-Type": "application/json" });
+          res.end(JSON.stringify(response));
+        } else {
+          // File not found error.
+          res.write("Nothing changed");
+        }
+      })
+      .listen(PORT, () => console.log(`server running at port: ${PORT}`));
+  } catch {
+    // Ensures that the client will close when you finish/error
+    console.log("Connection closing");
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
